0a5178496571d93358ddbf95af081c5294b46891
ashusatp <92241390+ashusatp@users.noreply.github.com>
2024-09-17 15:30:18 +0530

    mongodb config

--- config/database.go
+++ config/database.go
@@ -1,1 +1,36 @@
 package config
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var DB *mongo.Client
+
+func ConnectDB() {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal("Failed to connect to MongoDB:", err)
+	}
+
+	DB = client
+	log.Println("Connected to MongoDB")
+
+}
+
+func GetCollection(collectionName string) *mongo.Collection {
+	return DB.Database("todo_api").Collection(collectionName)
+}
